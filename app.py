fdd5999c66b20edef01a90f10f1818a709e08a14
Abdullah Albahar <a.albahar@gmail.com>
2025-05-15 08:22:34 +0300

    Reordered and removed pages, added logo and landing page content

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import streamlit as st
+from PIL import Image
 
-st.set_page_config(page_title="💼 Wealth Tracker", layout="centered")
+st.set_page_config(page_title=" Wealth Tracker", layout="centered")
 
-st.title("Investment Tracker App")
-st.markdown("""
-This App offers a clean dashboard for monitoring multi-currency portfolios, dividends, and exchange rates—designed with accuracy and future automation in mind.
+# Load and display the image
+image = Image.open("assets/logo.png")
+st.image(image, use_container_width=True)
 
+st.title("Track, plan, and grow—wisely.")
+st.markdown("""Treviwise offers a clean dashboard for monitoring multi-currency portfolios, dividends, and exchange rates, along with personal finance tools for complete control.
+            
 **👉 Use the sidebar to navigate between pages.**
             
 - 📊 Track your holdings and performance
@@ -13,5 +17,7 @@ This App offers a clean dashboard for monitoring multi-currency portfolios, divi
 - 📈 Visualize your portfolio over time
 - 🧾 Prepare for audits or taxes
 - 💰 Monitor dividends and income
-            
+
+This app was born out of my own need for precise, audit-grade financial records. It pushed me to explore Python, PostgreSQL, API integration, and performance tuning in Streamlit. I continue to refine it as I learn more about financial modeling and app deployment.
+
 """)
